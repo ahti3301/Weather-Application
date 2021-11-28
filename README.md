660d623a8117f5ebb33d59f6b58c64672162016f
ahti3301 <ahte02254@gmail.com>
2021-11-28 12:37:23 +0500

    added all files

---
+++ README.md
@@ -0,0 +1,18 @@
+# **_Weather-Application_**
+
+In this application i fetched data asynchronously from a weather Api (developer.accuweather.com) which tells the complete weather details of a given city or country
+
+## Features:
+
+- [x] tells the current temperature in centigrades
+- [x] tells the discription of weather i-e clouds, sky condition, day or night status
+- [x] humidity percentage
+- [x] wind speed in km/h
+- [x] change background image with respect to day and night status of the city
+
+## Tech:
+
+- Html
+- Css
+- Asynchronous Javascript
+- Bootstrap 5
---
+++ app.js
@@ -0,0 +1,98 @@
+//
+
+//console.log("checking");
+// (developer.accuweather.com) this is the api from which i am fetched my data
+const heading = document.querySelector(".main-head");
+const temperature = document.querySelector(".temperature");
+const iconImg = document.querySelector(".icon");
+const des = document.querySelector(".discription");
+const hum = document.querySelector(".humidity");
+const wind = document.querySelector(".wind");
+const form = document.querySelector("form");
+const bg = document.querySelector(".background");
+const p = document.querySelector("p");
+const line = document.querySelector(".line");
+let city;
+
+const apiKey = "E99zmmmutPiFo5mWbyOMxtGa7XKvdQFW"; //this is my api key which accuweather gave in a reult of creating my app
+const getCity = async (city) => {
+  const base = `http://dataservice.accuweather.com/locations/v1/cities/search`;
+  const query = `?apikey=${apiKey}&q=${city}`;
+  const response = await fetch(base + query);
+  const cityData = await response.json();
+
+  return cityData[0];
+  // 0 th index wali key ko ham prefer krengy hamesha
+};
+const getWeather = async (cityKey) => {
+  const base = `http://dataservice.accuweather.com/currentconditions/v1/`;
+  const query = `${cityKey}?apikey=${apiKey}&details=true`;
+  const response = await fetch(base + query);
+  const weatherDetails = await response.json();
+
+  return weatherDetails;
+  //console.log(weatherDetails);
+};
+
+const displayWeather = (weatherDetails) => {
+  const {
+    WeatherText,
+    WeatherIcon,
+    IsDayTime,
+    Temperature,
+    RelativeHumidity,
+    Wind,
+  } = weatherDetails[0];
+
+  console.log(
+    city,
+    WeatherText,
+    WeatherIcon,
+    IsDayTime,
+    Temperature.Metric.Value,
+    RelativeHumidity,
+    Wind.Speed.Metric.Value
+  );
+
+  // --------------------- send this data in our dom
+  heading.innerHTML = city;
+  temperature.innerHTML = `${Temperature.Metric.Value}&deg;C`;
+  iconImg.innerHTML = `<img src="./weather icons/${WeatherIcon}.png" alt="icon">`;
+  // day/night condition
+  let display;
+  if (IsDayTime == true) {
+    display = "Day";
+    bg.style.background =
+      "url(./images/day1.jpeg) no-repeat right bottom/cover";
+    p.style.color = "goldenrod";
+    line.style.backgroundColor = "goldenrod";
+  } else {
+    display = "Night";
+    bg.style.background =
+      "url(./images/night.jpeg) no-repeat center center/cover";
+    p.style.color = "black";
+    line.style.backgroundColor = "black";
+  }
+
+  des.innerHTML = `<span>${WeatherText}</span> <span>${display}</span>`;
+  hum.innerHTML = `Humidity :  ${RelativeHumidity} %`;
+  wind.innerHTML = `Wind : ${Wind.Speed.Metric.Value} km/h`;
+  const weather = document.querySelector(".weather");
+  weather.style.display = "block";
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  city = e.target.search.value;
+  form.reset();
+  getCity(city)
+    .then((data) => {
+      return getWeather(data.Key);
+    })
+    .then((weatherDetails) => {
+      return displayWeather(weatherDetails);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
